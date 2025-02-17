2b6fc822b9c27521f4d8795f625da5bfdda10f88
Niklas P <contact@niklasplessing.net>
2025-02-17 23:21:50 +0100

    init

---
+++ app/layout.tsx
@@ -0,0 +1,55 @@
+import { Footer, Layout, Navbar } from "nextra-theme-docs";
+import { Banner, Head } from "nextra/components";
+import { getPageMap } from "nextra/page-map";
+import "nextra-theme-docs/style.css";
+
+import "./globals.scss";
+
+export const metadata = {
+  // Define your metadata here
+  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+};
+
+const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>;
+const navbar = (
+  <Navbar
+    logo={<b>Nextra</b>}
+    // ... Your additional navbar options
+  />
+);
+const footer = <Footer>MIT {new Date().getFullYear()} © Nextra.</Footer>;
+
+export default async function RootLayout({ children }) {
+  const pageMap = await getPageMap();
+  return (
+    <html
+      lang="en"
+      dir="ltr"
+      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+      suppressHydrationWarning
+    >
+      <Head
+      // ... Your additional head options
+      >
+        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+      </Head>
+      <body className="min-h-screen dark:bg-gradient-to-b dark:from-purple-900 dark:via-black dark:to-purple-400 font-sans antialiased">
+        <Layout
+          banner={banner}
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/shuding/nextra/tree/main/docs"
+          footer={footer}
+          sidebar={{ autoCollapse: true, defaultOpen: true }}
+          navigation={{
+            prev: true,
+            next: true,
+          }}
+        >
+          {children}
+          {/* <pre>{JSON.stringify(pageMap, null, 2)}</pre> */}
+        </Layout>
+      </body>
+    </html>
+  );
+}
